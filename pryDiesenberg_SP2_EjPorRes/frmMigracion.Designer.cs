f49748d7713bfe0327f2715b4b981e356bcdfa6e
mmartidiesenberg <mmmartidiesenberg@gmail.com>
2026-04-28 09:37:55 -0300

    lisgo

--- pryDiesenberg_SP2_EjPorRes/frmMigracion.Designer.cs
+++ pryDiesenberg_SP2_EjPorRes/frmMigracion.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMigracion));
             this.btnIniciar = new System.Windows.Forms.Button();
             this.txtInformacion = new System.Windows.Forms.RichTextBox();
             this.lblInformacion = new System.Windows.Forms.Label();
@@ -72,6 +73,7 @@
             this.Controls.Add(this.lblInformacion);
             this.Controls.Add(this.txtInformacion);
             this.Controls.Add(this.btnIniciar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmMigracion";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Migración de Datos";
