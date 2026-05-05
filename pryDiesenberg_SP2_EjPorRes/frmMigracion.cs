73d1f682338e13a18141270737d20baa3effe797
mmartidiesenberg <mmmartidiesenberg@gmail.com>
2026-05-05 09:38:50 -0300

    .

--- pryDiesenberg_SP2_EjPorRes/frmMigracion.cs
+++ pryDiesenberg_SP2_EjPorRes/frmMigracion.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//
 namespace pryDiesenberg_SP2_EjPorRes
 {
     public partial class frmMigracion : Form
